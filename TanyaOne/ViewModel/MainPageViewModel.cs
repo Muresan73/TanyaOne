206bd6133fb80c7b448bbb0b7ea0502bbc0905e5
modig <modig@DESKTOP-EKF2IA0>
2016-10-01 10:33:49 +0200

    State in Grid

---
+++ TanyaOne/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TanyaOne.ViewModel
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public int  Value { get; set; }
+    }
+
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            ChartData = new List<Record>
+            {
+                new Record() {Name = "alma", Value = 20},
+                new Record() {Name = "alma", Value = 22},
+                new Record() {Name = "alma", Value = 23}
+            };
+        }
+
+        public List<Record> ChartData { get; set; }
+
+       
+
+    }
+}
