6b8951004003bf0e05f2ad14dcac5ae51440afbe
modig <modig@DESKTOP-EKF2IA0>
2016-11-21 19:17:25 +0100

    charter

---
+++ TanyaOne/View/ChartControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TanyaOne.ViewModel;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TanyaOne.View
+{
+    public sealed partial class ChartControl : UserControl
+    {
+        //public ChartViewModel CViewModel => (ChartViewModel) this.DataContext;
+        public ChartViewModel CViewModel { get; }
+
+        public ChartControl()
+        {
+            this.InitializeComponent();
+            CViewModel = new ChartViewModel();
+            (LineChart2.Series[0] as LineSeries).ItemsSource = CViewModel.VisualDataElements;
+
+            (LineChart2.Series[0] as LineSeries).IndependentAxis =
+                new CategoryAxis
+                {
+                    Orientation = AxisOrientation.X,
+                    Height = 0
+                };
+        }
+    }
+}
---
+++ TanyaOne/ViewModel/ChartViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TanyaOne.Model;
+
+namespace TanyaOne.ViewModel
+{
+    public class ChartViewModel
+    {
+        public bool isValid { get; set; }
+        public List<Datum> VisualDataElements { get; set; }
+        public Tile[] ChartTiles { get; set; }
+
+        public ChartViewModel()
+        {
+            var visualDataElements = new Datum[]{ new Datum() { date = "k0rte", value = 3 }, new Datum() { date = "cser", value = 4 }, new Datum() { date = "alma", value = 2 }, new Datum() { date = "has", value = 1 } };
+            var chartTiles = new[] { new Tile() { text = "08:88", value = "5" }, new Tile() { text = "08:90", value = "10" }, new Tile() { text = "09:88", value = "8" } };
+
+            UpdateChartData(visualDataElements,chartTiles);
+
+        }
+
+        public void UpdateChartData(Datum[] dates, Tile[] tiles)
+        {
+            ChartTiles = new Tile[4];
+            for (int i = 0; i < 4; i++)
+            {
+                ChartTiles[i] = tiles.Length > i ? tiles[i] : new Tile() {text = "", value = ""};
+            }
+            VisualDataElements = new List<Datum>(dates);
+        }
+    }
+}
